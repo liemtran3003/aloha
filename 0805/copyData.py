fa15a2982dc7b623c8fc6691f1fdd2d56e859eee
liemtran3003 <87455569+liemtran3003@users.noreply.github.com>
2021-08-05 13:56:40 +0900

    Update copyData.py

--- 0805/copyData.py
+++ 0805/copyData.py
@@ -5,8 +5,9 @@ wb = openpyxl.load_workbook("0805.xlsx")
 ws_hello = wb["hello"]
 ws_hello2 = wb["hello2"]
 
-for i in range(1, 2):
-	for n in range(1,8):
-		copydata = ws.cell(row = i, column = n).value
-	ws_hello2 = print(copydata)
-wb.save("0805.xlsx")
+for i in range(1, 9):
+	for n in range(1,3):
+		copydata = ws_hello.cell(row = i, column = n)
+		ws_hello2.cell(row = i, column = n).value = copydata.value
+
+wb.save("0805.xlsx") 
